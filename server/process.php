bc92e903c7b20baf3eeebee179342fdaff916f03
Leonard Mpande <leo.august27@gmail.com>
2015-12-10 16:40:32 +0300

    test callback

--- server/process.php
+++ server/process.php
@@ -21,7 +21,8 @@ if(isset($_GET["file"])) { $test_variable = "for upload";
 		}else{
 
 		if($dhp::uploadFile($_FILES["file"]["tmp_name"],$target_file)){
-            echo "UPLOAD_SUCCESS";
+//            echo "UPLOAD_SUCCESS";
+            echo json_encode($dhp::uploadFile($_FILES["file"]["tmp_name"],$target_file));
 		   }else{
 //            echo "UPLOAD_FAILED";
 //            echo $_FILES['file']['error'];
