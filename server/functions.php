000cb84eb8d460399e7d9c0bdb5db6a8f45d68b4
Leonard Mpande <leo.august27@gmail.com>
2016-01-13 20:13:42 +0300

    adding the auto

--- server/functions.php
+++ server/functions.php
@@ -77,9 +77,9 @@ class DhpFile {
 						$file_name	    =	substr($field_value, 0, strlen($field_value)-4);
 						}
 
-echo $value;
-					if(strpos($value,'Page',0)<0){
 
+					if(strpos($value,'Page',0)<0){
+                        echo $value;
 							if(strpos($field_value,"content here...")>=0){
                                 echo array_push($json_object,array($value=> ""));
 								}else{
